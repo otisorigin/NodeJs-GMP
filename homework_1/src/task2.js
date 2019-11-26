1c1897dbaf50643ccdbaa0a8061236947fb83e8c
fiodar.rymarovich <Fiodar_Rymarovich@epam.com>
2019-11-26 15:19:42 +0300

    task2 in progress

--- homework_1/src/task2.js
+++ homework_1/src/task2.js
@@ -1,1 +1,17 @@
-console.log("task 2 started");
+const csv = require('csvtojson')
+
+const csvFilePath = 'data/table.csv'
+const filePath = '/data/output.json'
+
+function onComplete() {
+    console.log('Всё гуд');
+}
+
+csv()   
+.fromFile(csvFilePath)
+.subscribe((json,lineNumber)=>{
+    console.log(lineNumber);
+    console.log(json);
+},(error) => console.log(error), onComplete)
+
+//const jsonArray=csv().fromFile(filePath);
