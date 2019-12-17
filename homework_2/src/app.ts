ed49eda0c198f7ba7890db6f47095010733129e9
fiodar.rymarovich <fiodar_rymarovich@epam.com>
2019-12-17 16:28:40 +0300

    Users moved to json file

--- homework_2/src/app.ts
+++ homework_2/src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import collection from "./collection";
+import usersData from "../data/users.json";
+import User from "./model/user";
 
 // Create Express server
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+const users = usersData.map((user) => new User(user.id, user.login, user.password, user.age));
+
 app.get("/", ( req, res ) => {
-    res.send( JSON.stringify(collection[0], null, ' ') );
+    res.send( "Hello world!" );
+    console.log(users);
 } );
 
 export default app;
--- homework_2/src/model/user.ts
+++ homework_2/src/model/user.ts
@@ -3,13 +3,12 @@ class User {
     login: string;
     password: string;
     age: number;
-    isDeleted: boolean;
+    isDeleted: boolean = false;
     constructor(id: string, login: string, password: string, age: number) {
         this.id = id;
         this.login = login;
         this.password = password;
         this.age = age;
-        this.isDeleted = false;
     }
 }
 
