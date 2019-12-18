b3adc5b104010fe4d1677233fd4c88c451e9dfa5
fiodar.rymarovich <fiodar_rymarovich@epam.com>
2019-12-18 15:45:30 +0300

    updated controller

--- homework_2/src/app.ts
+++ homework_2/src/app.ts
@@ -1,12 +1,23 @@
 import express from "express";
-import User from "./models/User";
+import bodyParser from "body-parser";
 
 import * as userController from "./controllers/user";
 
 // Create Express server
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 app.set("port", process.env.PORT || 3000);
-app.get("/users/", userController.getUsers);
+
+app.get("/users/", userController.findAllUsers);
+app.get("/users/:id", userController.findUser);
+app.post("/users/", userController.createUser);
+app.put("/users/:id", userController.updateUser);
+app.delete("/users/:id", userController.deleteUser);
 
 export default app;
--- homework_2/src/controllers/user.ts
+++ homework_2/src/controllers/user.ts
@@ -6,11 +6,45 @@ const users = usersData.map(
   user => new User(user.id, user.login, user.password, user.age)
 );
 
+//добавить обработку ошибок!
+//добавить валидацию
+//добавить getAll с limit
+
 /**
  * GET /users/
  * Get all users.
  */
-export const getUsers = (req: Request, res: Response) => {
-users[0].isDeleted = true;
+export const findAllUsers = (req: Request, res: Response) => {
   res.send(users.filter(user => user.isDeleted == false));
+};
+
+/**
+ * GET /users/id
+ * Get user by id.
+ */
+export const findUser = (req: Request, res: Response) => {
+  const id = req.params.id;
+  res.send(users.find(user => user.isDeleted == false && user.id == id));
+};
+
+export const updateUser = (req: Request, res: Response) => {
+  const id = req.params.id;
+  const user = users.find(user => user.isDeleted == false && user.id == id);
+  user.age = req.body.age;
+  user.login = req.body.login;
+  user.password = req.body.password;
+  res.sendStatus(200);
+};
+
+export const createUser = (req: Request, res: Response) => {
+  const user = new User(req.body.id, req.body.login, req.body.password, req.body.age);
+  users.push(user);
+  res.sendStatus(200);
+};
+
+export const deleteUser = (req: Request, res: Response) => {
+  const id = req.params.id;
+  const user = users.find(user => user.isDeleted == false && user.id == id);
+  user.isDeleted = true;
+  res.sendStatus(200);
 };
