472b0e77cbe215308d4b3911d1f5df25f3c257df
fiodar.rymarovich <fiodar_rymarovich@epam.com>
2020-02-13 17:40:32 +0300

    added return types

--- homework_5/src/data-access/groupRepository.ts
+++ homework_5/src/data-access/groupRepository.ts
@@ -1,26 +1,32 @@
 import Group from '../models/Group';
+import User from '../models/User';
 import GroupDTO from '../util/dto/GroupDTO';
 import sequelize from '../loaders/sequelize';
 
-export const findAllGroups = () => Group.findAll({ raw: true });
+export const findAllGroups = (): Promise<Group[]> => Group.findAll({ raw: true });
 
-export const findGroupById = (groupId: number) => Group.findByPk(groupId);
+export const findGroupById = (groupId: number): Promise<Group> =>
+    Group.findByPk(groupId);
 
-export const removeGroup = (groupId: number) =>
+export const removeGroup = (groupId: number): Promise<number> =>
     Group.destroy({ where: { id: groupId } });
 
-export const updateGroup = (updatedGroup: GroupDTO) =>
+export const updateGroup = (updatedGroup: GroupDTO): Promise<Group> =>
     Group.findByPk(updatedGroup.id).then(group => group.update(updatedGroup));
 
-export const createGroup = (newGroup: GroupDTO) => Group.create(newGroup);
+export const createGroup = (newGroup: GroupDTO): Promise<Group> =>
+    Group.create(newGroup);
 
-export const addUsersToGroup = (groupId: number, userIds: number[]) =>
+export const addUsersToGroup = (
+    groupId: number,
+    userIds: number[]
+): Promise<void> =>
     sequelize.transaction(async t =>
         Group.findByPk(groupId).then(group =>
             group.addUsers(userIds, { transaction: t })
         )
     );
 
-export const findGroupUsers = (groupId: number) => {
+export const findGroupUsers = (groupId: number): Promise<User[]> => {
     return Group.findByPk(groupId).then(group => group.getUsers());
 };
