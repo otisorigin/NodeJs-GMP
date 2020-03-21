b4428736a42f87e5c547fad180e907a5107f2b9d
fedor <fedorrimarovich@gmail.com>
2020-03-21 16:28:40 +0300

    added jest

--- homework_7/src/api/controllers/authController.ts
+++ homework_7/src/api/controllers/authController.ts
@@ -1,38 +1,38 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import * as service from '../../services/userService';
-import UnauthorizedException from '../../types/exceptions/UnauthorizedException';
-import log from '../../utils/winston';
-import validator from '../middlewares/requestValidator';
-import authSchema from '../../types/schemas/authSchema';
-import UserDTO from '../../types/dto/UserDTO';
+import { Router, Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import * as service from "../../services/userService";
+import UnauthorizedException from "../../types/exceptions/UnauthorizedException";
+import log from "../../utils/winston";
+import validator from "../middlewares/requestValidator";
+import authSchema from "../../types/schemas/authSchema";
+import UserDTO from "../../types/dto/UserDTO";
 
 const route = Router();
 
 const generateToken = (user: UserDTO): string => {
-    const payload = { id: user.id, age: user.age };
-    return jwt.sign(payload, 'secret', { expiresIn: 30 });
+  const payload = { id: user.id, age: user.age };
+  return jwt.sign(payload, process.env.SECRET_WORD, { expiresIn: 30 });
 };
 
 const login = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> => {
-    try {
-        const user = await service.findUserByLogin(req.body.login);
-        if (!user || user.password !== req.body.password) {
-            throw new UnauthorizedException('Incorrect login or password');
-        } else {
-            const token = generateToken(user);
-            res.send({ token });
-        }
-    } catch (err) {
-        log.info(`Catched exception in: ${login} ${module.filename}`);
-        next(err);
+  try {
+    const user = await service.findUserByLogin(req.body.login);
+    if (!user || user.password !== req.body.password) {
+      throw new UnauthorizedException("Incorrect login or password");
+    } else {
+      const token = generateToken(user);
+      res.send({ token });
     }
+  } catch (err) {
+    log.info(`Catched exception in: ${login} ${module.filename}`);
+    next(err);
+  }
 };
 
-route.post('/', validator(authSchema), login);
+route.post("/", validator(authSchema), login);
 
 export default login;
