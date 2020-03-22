ad88cd7b586dcc7b0e9b51a1fde69d38c7013fba
fedor <fedorrimarovich@gmail.com>
2020-03-23 01:16:23 +0300

    finished 8 homewrok

--- homework_7/src/api/middlewares/tokenValidator.ts
+++ homework_7/src/api/middlewares/tokenValidator.ts
@@ -1,27 +1,27 @@
-import { Request, NextFunction, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const tokenValidator = (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): void => {
-    const token = req.headers['x-access-token'] as string;
-    if (token) {
-        jwt.verify(token, process.env.SECRET_WORD, err => {
-            if (err) {
-                res.status(403).send({
-                    message: 'Invalid token.'
-                });
-            } else {
-                next();
-            }
-        });
-    } else {
+  const token = req.headers["x-access-token"] as string;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_WORD, err => {
+      if (err) {
         res.status(401).send({
-            message: 'No token provided.'
+          message: "Access token is missing or invalid."
         });
-    }
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({
+      message: "Access token is missing or invalid."
+    });
+  }
 };
 
 export default tokenValidator;
